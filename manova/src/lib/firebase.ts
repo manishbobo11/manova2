bbe66b09a5d807ee0e0dead0df503d3159cec12a
Manish <ms@Manishs-MacBook-Air.local>
2025-08-15 20:17:28 +0530

    final backend push for render deploy

--- manova/src/lib/firebase.ts
+++ manova/src/lib/firebase.ts
@@ -2,12 +2,12 @@ import { initializeApp, getApps } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    VITE_FIREBASE_API_KEY=AIzaSyAB69J1WxYLd_vwxggx8YJb37YtTr7kePU
+    VITE_FIREBASE_AUTH_DOMAIN=manova-9b8ea.firebaseapp.com
+    VITE_FIREBASE_PROJECT_ID=manova-9b8ea
+    VITE_FIREBASE_STORAGE_BUCKET=manova-9b8ea.appspot.com
+    VITE_FIREBASE_MESSAGING_SENDER_ID=107629287941
+    VITE_FIREBASE_APP_ID=1:107629287941:web:9c3ab2f6e6bf571474da6c,
 };
 
 const app = getApps().length ? getApps()[0] : initializeApp(firebaseConfig);
